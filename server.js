2f47b5b2c1cd9bed91f428becfa49e7ae7b2f5eb
marisanesmith <marisa@thisworldtraveled.com>
2021-04-18 20:16:24 -0400

    updated server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budgetDB", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budgetDB", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
